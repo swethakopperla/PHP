2109a3884360965bdc0c5433fea0dc55035b60e2
swethakopperla <swethak.itblabs@gmail.com>
2017-12-07 18:10:52 +0530

    echo examples

---
+++ echo.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<h1>
+echo statements
+<h1><p>
+<?php
+echo "<h2> PHP is fun!</h2>";
+echo "Hello world</br>";
+echo "this","is","very","easy";
+?>
+</p>
+</body>
+</html>
+
---
+++ echo2.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<h1> echo example</h1>
+<?php
+$txt="hello";
+$txt2="goddmoring";
+$x=9;
+$y=10;
+echo "<h4>".$txt."</h4>";
+echo "study PHP At".$txt."</br>";
+echo $x+$y;
+?>
+</body>
+</html>
+
