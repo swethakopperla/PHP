e50159c4d094e1cdfee7b1e71031071b03b08a63
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2017-12-07 19:03:06 +0530

    Update string.php

--- string.php
+++ string.php
@@ -14,6 +14,8 @@ echo str_word_count($txt);
   echo strpos("Hello world!", "world");
   echo "<br>";
   echo strpos($txt, "world");
+   echo "<br>";
+  echo str_replace("world", "Dolly",$txt);
 ?>
 </body>
 </html>
