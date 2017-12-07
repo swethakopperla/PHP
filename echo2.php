38ea12665704a55e997467c3838f6fbdc768a6fb
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2017-12-07 18:16:44 +0530

    Update echo2.php

--- echo2.php
+++ echo2.php
@@ -3,11 +3,11 @@
 <h1> echo example</h1>
 <?php
 $txt="hello";
-$txt2="goddmoring";
+$txt2="godd moring";
 $x=9;
 $y=10;
 echo "<h4>".$txt."</h4>";
-echo "study PHP At".$txt."</br>";
+echo "study PHP At".$txt2."</br>";
 echo $x+$y;
 ?>
 </body>
