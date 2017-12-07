94878c8388fdf1f6a2e06e49d6a3ea6d42fb2a7e
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2017-12-07 20:24:05 +0530

    Update database.php

--- database.php
+++ database.php
@@ -9,7 +9,7 @@ if ($con->connect_error)
 {
 die('Could not connect: ' . $con->connect_error);
 }echo "connected successfully";
-$sql = "CREATE DATABASE DBSWE";
+$sql = "CREATE DATABASE DBSWE;";
 if ($conn->query($sql) === TRUE) {
     echo "Database created successfully";
 } else {
