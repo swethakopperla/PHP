ed392237836c342c47514f50f7a485d5ff36db0d
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2017-12-07 19:45:24 +0530

    updated

--- database.php
+++ database.php
@@ -9,6 +9,14 @@ if (!$con)
 {
 die('Could not connect: ' . mysql_error());
 }
+  if (mysql_query("CREATE DATABASE database1",$con))
+{
+echo "Database created";
+  }
+  else
+  {
+    echo "Error creating database: " . mysql_error();
+  }
 // some code
 mysql_close($con);
 ?>
