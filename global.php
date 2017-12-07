3dff735de268f31f3c19b3afdc41311747ecf4be
swethakopperla <swethak.itblabs@gmail.com>
2017-12-07 16:04:45 +0530

    sec php update

---
+++ global.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+$x=5;
+function myText(){
+echo"<p>variable x is global:$x";
+}
+myText();
+$text="hello world";
+echo "<p>variable x is global:$text";
+?>
+</body>
+</html>
+
---
+++ var2.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h1> output variables</h1>
+<?php
+$txt="hello world!";
+echo "good moring".$txt;
+?>
+</body>
+</html>
+
