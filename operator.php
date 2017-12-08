c8082fa27eed03ee27773d57991973c71953770e
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2017-12-08 12:39:56 +0530

    Update operator.php

--- operator.php
+++ operator.php
@@ -4,8 +4,8 @@
 <?php
 $x=10;
 $y=8;
-echo "sum".$x+$y;echo "<br>";
-echo "sub".$x-$y;echo "<br>";
+echo $x+$y;echo "<br>";
+echo $x-$y;echo "<br>";
 echo "mul".$x*$y;echo "<br>";
 echo "div".$x/$y;echo "<br>";
   echo $x=$y;echo "<br>";
