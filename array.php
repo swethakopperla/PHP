41675766000532ae6cd70bcfe0496f23d4e71df7
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2017-12-08 19:59:50 +0530

    updated

--- array.php
+++ array.php
@@ -2,6 +2,10 @@
 <body>
 <h1>
 php arrays
-<h1>
+</h1>
+  <?php
+$cars = array("Volvo", "BMW", "Toyota"); 
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
 </body>
 </html>
