49c3a1c554ce4de7319232571d901625667215e0
swethakopperla <swethak.itblabs@gmail.com>
2017-12-13 12:38:18 +0530

    updated

--- para2.php
+++ para2.php
@@ -1,9 +1,5 @@
 <html>
 <body>
-<form method="post">
-Enter name:<input type="text" name="name"></br>
-Enter email:<input type="text" name="email"></br>
-<input type="submit" name="add">
 <?php
  function name($name,$email)
 {
