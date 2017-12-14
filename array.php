b385af05a2a39191e8e8fdb025a4a30e2f59b378
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2017-12-14 10:36:19 +0530

    Update array.php

--- array.php
+++ array.php
@@ -6,6 +6,10 @@ php arrays
   <?php
 $cars = array("Volvo", "BMW", "Toyota"); 
 echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+  $season[0]="SUMMER";
+  $season[1]="SPRIGS";
+  $season[2]="WINTER";
+  echo "seasons are : $season[0],$season[1],$season[2]";
 ?>
 </body>
 </html>
