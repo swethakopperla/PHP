d24f99a3b51ea27bea4458533625326314ea6208
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2017-12-17 15:29:34 +0530

    Update database.php

--- database.php
+++ database.php
@@ -8,7 +8,7 @@ $con =new mysqli("localhost","wordpressuser","password");
 if ($con->connect_error)
 {
 die('Could not connect: ' . $con->connect_error);
-}echo "connected successfully";
+}echo "word press user connected successfully";
 $sql = "CREATE DATABASE DBSWE;";
 if ($conn->query($sql) === TRUE) {
     echo "Database created successfully";
