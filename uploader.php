cabf2193052b68c18c9341642f64d16b6e4db8a5
swethakopperla <swethak.itblabs@gmail.com>
2017-12-17 18:29:07 +0530

    uploadform

---
+++ uploader.php
@@ -0,0 +1,12 @@
+<html>
+<?php  
+$target_path = "e:/";  
+$target_path = $target_path.basename( $_FILES['fileToUpload']['name']);   
+  
+if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path)) {  
+    echo "File uploaded successfully!";  
+} else{  
+    echo "Sorry, file not uploaded, please try again!";  
+}  
+?> 
+</html>
