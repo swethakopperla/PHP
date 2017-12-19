c992cf7e783d509c05823fb24621216a8022335a
swethakopperla <33566730+swethakopperla@users.noreply.github.com>
2017-12-19 12:30:29 +0530

    Update selectall.php

--- selectall.php
+++ selectall.php
@@ -5,7 +5,7 @@ $host="localhost";
 $user="root";
 $pass="Root@1234";
 $dbname="swetha";
-$conn=mysqli_connet($host,$user,$pass,$dbname);
+$conn=mysqli_connect($host,$user,$pass,$dbname);
 if(!$conn)
 die("could not connect:".mysqli_connect_error());
 else
