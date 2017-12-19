513e802373e71e0156422664eefb6b2fdea40709
swethakopperla <swethak.itblabs@gmail.com>
2017-12-19 12:22:46 +0530

    selectall.php

---
+++ selectall.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+$host="localhost";
+$user="root";
+$pass="Root@1234";
+$dbname="swetha";
+$conn=mysqli_connet($host,$user,$pass,$dbname);
+if(!$conn)
+die("could not connect:".mysqli_connect_error());
+else
+echo "connected sucessfully<br>";
+?>
+</body>
+</html>
+
