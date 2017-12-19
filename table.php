99970af85f56032e6107797566da015a8ee4a9a9
swethakopperla <swethak.itblabs@gmail.com>
2017-12-19 11:27:51 +0530

    table.php

---
+++ table.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+<?php  
+$host = 'localhost:3306';  
+$user = 'root';  
+$pass = 'Root@1234';  
+$dbname = 'swetha';  
+  
+$conn = mysqli_connect($host, $user, $pass,$dbname);  
+if(!$conn){  
+  die('Could not connect: '.mysqli_connect_error());  
+}  
+echo 'Connected successfully<br/>';  
+  
+$sql = "create table emp5(id INT AUTO_INCREMENT,name VARCHAR(20) NOT NULL,  
+emp_salary INT NOT NULL,primary key (id))";  
+if(mysqli_query($conn, $sql)){  
+ echo "Table emp5 created successfully";  
+}else{  
+echo "Could not create table: ". mysqli_error($conn);  
+}  
+  
+mysqli_close($conn);  
+?>
+</body>
+</html>
