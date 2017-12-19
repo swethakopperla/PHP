571fb8fdb11dac9003bcb3d16400f581c0acdecf
swethakopperla <swethak.itblabs@gmail.com>
2017-12-19 10:39:21 +0530

    connection.php

---
+++ connection.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<?oho
+$hostname="localhost";
+$username="root";
+$password="Root@1234";
+$dbname="swetha";
+$usertable="empdtls";
+$yourfield = "id";
+$connection = mysql_connect($hostname, $username, $password);
+mysql_select_db($dbname, $connection);
+
+# Check If Record Exists
+
+$query = "SELECT * FROM $usertable";
+
+$result = mysql_query($query);
+
+if($result)
+{
+while($row = mysql_fetch_array($result))
+{
+$name = $row["$yourfield"];
+echo "Name: ".$name."
+";
+}
+}
+?>
+</body>
+</html>
